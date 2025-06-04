e580f0c742427f797b8932e01123a02b6d763e8b
Larissa <larissahamlet@gmail.com>
2025-06-04 00:20:27 -0300

    tentando fazer deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,12 @@ const dev = process.env.NODE_ENV === 'development';
 
 export default {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // this tells SvelteKit to emit a single-page "index.html" fallback
+      fallback: 'index.html'
+    }),
     paths: {
-      base:  dev ? '' : '/final-project-history-of-philosophy'
+      base: dev ? '' : '/final-project-history-of-philosophy'
     }
   }
 };
