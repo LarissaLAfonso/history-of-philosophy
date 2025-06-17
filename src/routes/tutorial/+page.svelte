eb29d7b298734be264fce608dba8029cb2087bb2
KaikyBraga <kaiky.braga06221607@gmail.com>
2025-06-16 22:32:18 -0300

    comentários

--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -2,7 +2,7 @@
   import './tutorial.css';
   import { base } from '$app/paths';
   import explanations from '../../components/data/tutorial_explanations.json';
-
+    
   let currentSlide = 0;
 
   const nextSlide = () => {
@@ -33,12 +33,12 @@
       <div class="slider-window">
         {#each explanations as exp, i}
           <div class="content-slide {i === currentSlide ? 'active' : ''}">
-            <!-- Coluna da Imagem -->
+            <!-- IMAGE COLUMN -->
             <div class="slide-image">
               <img src={exp.gif} alt="explicação visual" />
             </div>
 
-            <!-- Coluna do Título + Texto -->
+            <!-- TITLE COLUMN + TEXT -->
             <div class="slide-text">
               <h2 class="slide-title">{exp.title}</h2>
               <div class="text-explanation">
